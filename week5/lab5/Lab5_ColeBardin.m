4cca00085e8a7af274e498540c18ffc4189a72a8
ColeBardin <cab572@drexel.edu>
2022-07-21 11:51:20 -0400

    almost done with problems 9-10

--- week5/lab5/Lab5_ColeBardin.m
+++ week5/lab5/Lab5_ColeBardin.m
@@ -1,2 +1,157 @@
 %% Lab 5 by Cole Bardin 		Section: 61
 %% Method of Undetermined Coefficients
+%% Part A: Homogeneous DE
+clc, clear, close all
+% Question 2
+syms y(t)
+Dy = diff(y,t); D2y = diff(y,t, t);
+
+DE = 4*D2y +4*Dy + 17*y == 0;
+sol = dsolve(DE, y(0)==4, Dy(0)==0)
+
+% Question 3
+
+Y = matlabFunction(sol);
+Dy = matlabFunction(diff(Y, t));
+
+Y(1)
+Dy(1)
+
+% Questions 4-5
+figure
+time = 0: 0.01 : 10;
+
+subplot(2,3,1) % tile 1
+set(gca,"FontSize",20)
+hold on
+grid on
+plot(time, Y(time), 'm', 'LineWidth',3)
+plot(0,4, 'bo', 'MarkerSize',8, 'MarkerFaceColor','g')
+title("Solution curve y(t)")
+xlabel("Time in seconds")
+ylabel("Y(t)")
+axis([0, 10, -4, 4])
+xticks(0:2:10)
+yticks(-4:2:4)
+
+subplot(2, 3, 4) % tile 4
+set(gca,"FontSize",20)
+hold on
+grid on
+plot(time, Dy(time), 'r', 'LineWidth',3)
+plot(0,0, 'bo', 'MarkerSize',8, 'MarkerFaceColor','g')
+title("Derivative of y(t)")
+xlabel("Time in seconds")
+ylabel("Dy(t)")
+axis([0, 10, -6, 6])
+xticks(0:2:10)
+yticks(-6:2:6)
+
+subplot(2, 3, [2, 3, 5, 6]) % 2-3 and 5-6
+set(gca,"FontSize",20)
+hold on
+grid on
+phase = plot(Y(time), Dy(time), 'k', 'LineWidth',3);
+center = plot(0, 0, 'bh', 'MarkerSize', 14, 'MarkerFaceColor', 'yellow'); % Center Point
+ic = plot(4,0, 'bo', 'MarkerSize',8, 'MarkerFaceColor','g');
+title("Phase Plot")
+xlabel("Y(t)")
+ylabel("Dy(t)")
+axis([-6, 6, -6, 6])
+xticks(-6:6)
+yticks(-6:6)
+legend([phase, center, ic], ["phase", "center", "IC"])
+%% Part B: Non-Homogeneous DE: Method of Undetermined Coefficients
+clc, clear, close all
+% Questions 6-7
+syms y(t)
+Dy = diff(y,t); D2y = diff(y,t, t);
+
+% a)
+DE = 4*D2y +4*Dy + 17*y == 17 + 289*t;
+sol = dsolve(DE, y(0)==4, Dy(0)==0);
+disp("a)")
+sol = simplify(sol)
+% b)
+DE = 4*D2y +4*Dy + 17*y == 100*exp(-2*t);
+sol = dsolve(DE, y(0)==4, Dy(0)==0);
+disp("b)")
+sol = simplify(sol)
+% c)
+DE = 4*D2y +4*Dy + 17*y == 260*cos(2*t);
+sol = dsolve(DE, y(0)==4, Dy(0)==0);
+disp("c)")
+sol = simplify(sol)
+% d)
+DE = 4*D2y +4*Dy + 17*y == 16*exp(-t/2)*cos(2*t) + 32*exp(-t/2)*sin(2*t);
+sol = dsolve(DE, y(0)==4, Dy(0)==0);
+disp("d)")
+sol = simplify(sol)
+
+%% Questions 8-10: The Method of Undetermined Coefficients from SCRATCH
+clc, clear, close all
+% Question 8
+syms A B t
+syms y(t) a b
+Dy = diff(y,t); D2y = diff(y,t, t);
+f = 260*cos(2*t)
+DE = 4*D2y + 4*Dy + 17*y - f == 0 % non-homogeneous differential equation
+
+Y = A * cos(2*t) + B * sin(2*t) % our guess
+plug_it_in = subs(DE, y, Y) % Plug our guess into the DE
+eqn = collect(plug_it_in, [cos(2*t), sin(2*t)]) % arrange by similar terms
+
+% MATLAB can find the undetermined coefficients for us
+equations = coeffs( lhs(eqn), [cos(2*t), sin(2*t)] )
+variables = [A, B]
+% find and display the undetermined coefficients
+[A, B] = solve(equations, variables) 
+
+% Questions 9-10
+Y = A * cos(2*t) + B * sin(2*t) % our guess;
+Dy = diff(Y, t)
+Y = matlabFunction(Y); % our guess);
+Dy = matlabFunction(Dy);
+
+fig3 = figure(3);
+time = 0: 0.01 : 10;
+
+subplot(2,3,1) % tile 1
+set(gca,"FontSize",20)
+hold on
+grid on
+plot(time, Y(time), 'm', 'LineWidth',3)
+plot(0,4, 'bo', 'MarkerSize',8, 'MarkerFaceColor','g')
+title("Solution curve y(t)")
+xlabel("Time in seconds")
+ylabel("Y(t)")
+axis([0, 10, -40, 40])
+xticks(0:2:10)
+yticks(-40:20:40)
+
+subplot(2, 3, 4) % tile 4
+set(gca,"FontSize",20)
+hold on
+grid on
+plot(time, Dy(time), 'r', 'LineWidth',3)
+plot(0,0, 'bo', 'MarkerSize',8, 'MarkerFaceColor','g')
+title("Derivative of y(t)")
+xlabel("Time in seconds")
+ylabel("Dy(t)")
+axis([0, 10, -60, 60])
+xticks(0:2:10)
+yticks(-50:50:50)
+
+subplot(2, 3, [2, 3, 5, 6]) % 2-3 and 5-6
+set(gca,"FontSize",20)
+hold on
+grid on
+phase = plot(Y(time), Dy(time), 'k', 'LineWidth',3);
+ic = plot(4,0, 'bo', 'MarkerSize',8, 'MarkerFaceColor','g');
+title("Phase Plot")
+xlabel("Y(t)")
+ylabel("dy/dt(t)")
+axis([-40, 40, -80, 80])
+xticks(-40:20:40)
+yticks(-60:20:60)
+legend([phase, ic], ["phase", "IC"])
