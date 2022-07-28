0a531a55936fa1c556ec26c5c5fd7594514af331
ColeBardin <cab572@drexel.edu>
2022-07-28 11:42:52 -0400

    all problems that used matlab are in the doc

--- week6/lab6/Lab6_ColeBardin.m
+++ week6/lab6/Lab6_ColeBardin.m
@@ -1,2 +1,26 @@
 %% Lab 6 by Cole Bardin 	Section 61
-%%  
+%% Laplace Workshop
+%% Question 1.2
+clc, clear
+AM = [1,1,0,-3;0,4,1,-14;4,0,4,32]
+rref(AM)
+
+%% Question 1.4
+clc, clear
+AM = [1,1,0,0;4,7,1,0;4,10,5,90]
+rref(AM)
+
+%% Question 1.7
+clc, clear
+AM = [1,1,1,1;6,2,4,7;8,0,0,5]
+rref(sym(AM))
+
+%% Question 1.8
+clc, clear
+AM = [1,1,1,0;5,7,8,0;6,10,15,5]
+rref(sym(AM))
+
+%% Question 1.9
+clc, clear
+AM = [1,0,1,1;6,1,0,0;0,6,0,1]
+rref(sym(AM))
